00db8192b208f3efbb09d8c037d0c8eedcf00264
Nikolas Praseutsinh <101019501+praseutsinh@users.noreply.github.com>
2025-10-01 13:43:19 -0500

    Cleaned up code

--- inMeanStd/inMeanStd.cpp
+++ inMeanStd/inMeanStd.cpp
@@ -1,76 +1,67 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+#include <array>
 
-// Function to calculate the mean of a set of four numbers
-float calculateMean(float set[]) {
-	float mean = 0;
-	for( int i = 0; i < 4; i++) {
-		//sums the numbers
-		mean += set[i];
+class stats {
+public:
+	// Function to calculate the mean of a set of four numbers
+	double calculateMean(double set[]) {
+		double sum = 0;
+		for (int i = 0; i < 4; i++) {
+			sum += set[i];
+		}
+		return sum / 4;
 	}
-	//divides the sum by 4 to get the mean
-	return mean/4;
-}
 
-// Function to calculate the standard deviation of a set of four numbers
-float standardDeviation(float set[]) {
-	// Calculate the mean
-	float mean = calculateMean(set);
-	//Population size or array size
-	int n = 4;
-	//instantiate variance variable
-	float variance = 0;
-	// Calculate the variance
-	for(int x = 0; x < n; x++){
-		float diff = set[x] - mean;
-		float sqrDiff = diff * diff;
-		variance = (variance + sqrDiff);
+	// Function to calculate the standard deviation of a set of four numbers
+	double standardDeviation(double set[]) {
+		const double mean = calculateMean(set);
+		double variance = 0;
+		for (int x = 0; x < 4; x++) {
+			double diff = set[x] - mean;
+			variance += diff * diff;
+		}
+		return  std::sqrt(variance/4);
 	}
-	//Divide by population size to get variance
-	variance = variance / n;
-	// Return the standard deviation
-	return  std::sqrt(variance);
-}
+};
 
 int main() {
+	stats calculate;
+	const int SIZE = 4;
 
 	//----------USER INPUT SECTION----------//
-	
-	//Array to hold the set of four numbers
-	float set[4] = {};
+	double userSet[SIZE] = {};
 	std::cout << "Enter a set of four integers: " << std::endl;
-	// Input the set of four numbers through a loop
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < SIZE; i++) {
 		std::cout << i + 1 << ") ";
-		std::cin >> set[i];
+		std::cin >> userSet[i];
 	}
-	// Output the mean and standard deviation
-	std::cout << "User Mean: " << calculateMean(set) << std::endl;
-	std::cout << "User Standard Deviation: " << standardDeviation(set) << std::endl;
+
+	std::cout << "User Mean: " << calculate.calculateMean(userSet) << std::endl;
+	std::cout << "User Standard Deviation: " << calculate.standardDeviation(userSet) << std::endl;
 
 	//----------FILE INPUT SECTION----------//
 	std::ifstream inputFile("inMeanStd.dat");
 	std::ofstream outputFile("outMeanStd.dat");
+	double fileSet[SIZE] = {};
 
-
-	float fileSet[4] = {};
-	// Check if the file opened successfully
 	if (!inputFile) {
 		std::cerr << "\nError opening file." << std::endl;
-
 	}
 	else {
-		//If the file opened successfully, read the numbers into the array
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < SIZE; i++) {
 			inputFile >> fileSet[i];
 		}
-		outputFile << "File Mean: " << calculateMean(fileSet) << std::endl;
-		std::cout << "\nFile Mean: " << calculateMean(fileSet) << std::endl;
+		outputFile << "File Mean: " << calculate.calculateMean(fileSet) << std::endl;
+		std::cout << "\nFile Mean: " << calculate.calculateMean(fileSet) << std::endl;
 
-		outputFile << "File Standard Deviation: " << standardDeviation(fileSet) << std::endl;
-		std::cout << "File Standard Deviation: " << standardDeviation(fileSet) << std::endl;
+		outputFile << "File Standard Deviation: " << calculate.standardDeviation(fileSet) << std::endl;
+		std::cout << "File Standard Deviation: " << calculate.standardDeviation(fileSet) << std::endl;
 	}
+
 	outputFile.close();
 	inputFile.close();
+
+	return 0;
 }
